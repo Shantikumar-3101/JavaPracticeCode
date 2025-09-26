b665ad27ba0a429cb83b17eeba7498276fdb6898
Shantikumar-3101 <94917245+Shantikumar-3101@users.noreply.github.com>
2025-09-26 08:20:01 +0530

    modified

--- src/JavaCodeingPractice/FindnearestPrime.java
+++ src/JavaCodeingPractice/FindnearestPrime.java
@@ -2,9 +2,34 @@ package JavaCodeingPractice;
 
 public class FindnearestPrime {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    public static void main(String[] args) {
+	        int number = 20;
+	        System.out.println("Closest prime to " + number + " is " + findClosestPrime(number));
+	    }
 
+	    public static int findClosestPrime(int number) {
+	        int lower = number - 1;
+	        int higher = number + 1;
+
+	        while (true) {
+	            if (isPrime(lower)) {
+	                return lower;
+	            }
+	            if (isPrime(higher)) {
+	                return higher;
+	            }
+	            lower--;
+	            higher++;
+	        }
+	    }
+
+	    public static boolean isPrime(int num) {
+	        if (num <= 1) return false;
+	        for (int i = 2; i <= Math.sqrt(num); i++) {
+	            if (num % i == 0) return false;
+	        }
+	        return true;
+	    }
 	}
 
-}
+
